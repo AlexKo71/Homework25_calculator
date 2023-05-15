25240f5495b7be1358cd4eb2ebe5ed9f01326658
Alexey Kolodin <alekko-sp@yandex.ru>
2023-05-15 14:09:32 +0300

    there is an empty branch here

---
+++ src/main/java/com/example/homework25_calculator/Homework25CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework25_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework25CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework25CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework25_calculator/Homework25CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework25_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework25CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
